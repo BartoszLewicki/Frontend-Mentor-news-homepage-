5045135e03f2351334bf2621c17a0c28bbdd6d55
BartoszLewicki <bartosz.lewicki.1m@gmail.com>
2022-11-13 20:04:21 +0100

    project done

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const menuButton = document.querySelector("#hamburger-menu");
 const closeButton = document.querySelector("#close-menu");
 const nav = document.querySelector("#navigation");
+const overlay = document.querySelector("#overlay");
 
 window.addEventListener("resize", () => {
   if (window.matchMedia("(max-width: 700px)").matches) {
@@ -16,10 +17,14 @@ menuButton.addEventListener("click", () => {
   nav.style.display = "flex";
   closeButton.classList.remove("hidden");
   closeButton.classList.add("visible");
+  overlay.classList.remove("hidden");
+  overlay.classList.add("visible");
 });
 
 closeButton.addEventListener("click", () => {
   nav.style.display = "none";
   closeButton.classList.remove("visible");
   closeButton.classList.add("hidden");
+  overlay.classList.remove("visible");
+  overlay.classList.add("hidden");
 });
